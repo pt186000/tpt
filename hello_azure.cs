8a8995682cb6369655dd0eaf32c87b61753554c8
Phong Ta <phong.ta@outlook.com>
2020-12-14 22:55:00 -0800

    Message Display

--- hello_azure.cs
+++ hello_azure.cs
@@ -27,7 +27,7 @@ namespace HelloAzureFunctions
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name} from serverless days ANZ. This HTTP triggered function executed successfully.";
+                : $"Hello, {name} from serverless days ANA. This HTTP triggered function executed successfully.";
 
             return new OkObjectResult(responseMessage);
         }
